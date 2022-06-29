db9ae7f6f27e7b9dbb7504476e2306d833e27aeb
RegistryROK <registry.forbusiness@gmail.com>
2022-06-30 04:16:19 +0900

    feat: title, list, info를 dailysales에 추가
    현재날짜를 title에 표시되게함.

--- pages/dailysales/index.tsx
+++ pages/dailysales/index.tsx
@@ -1,11 +1,15 @@
 /** @jsxImportSource @emotion/react **/
 import { css } from '@emotion/react';
-import { AppBar, Button, Container } from '@mui/material';
+import { AppBar, Button, Container, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 
 export default function DailySales() {
   const router = useRouter();
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
   return (
     <>
       <AppBar css={styles.appBar} elevation={1}>
@@ -21,7 +25,14 @@ export default function DailySales() {
           <Image src='/images/back.svg' width={24} height={24} />
         </Button>
       </AppBar>
-      <Container css={styles.contents} maxWidth={false} disableGutters />
+      <Container css={styles.contents} maxWidth={false} disableGutters>
+        <Typography css={styles.title}>
+          당일 매출 – {year}년 {month < 10 ? `${0}${month}` : month}월{' '}
+          {day < 10 ? `${0}${day}` : day}일
+        </Typography>
+        <Container css={styles.list} maxWidth={false} disableGutters />
+        <Container css={styles.info} maxWidth={false} disableGutters />
+      </Container>
     </>
   );
 }
@@ -71,5 +82,27 @@ const styles = {
     height: 994px;
     left: 0;
     top: 86px;
+  `,
+  title: css`
+    position: absolute;
+    left: 32px;
+    top: 30px;
+    font-size: 36px;
+    font-family: Roboto;
+  `,
+  list: css`
+    position: absolute;
+    width: 1260px;
+    height: 920px;
+    left: 0;
+    bottom: 0;
+  `,
+  info: css`
+    position: absolute;
+    width: 660px;
+    height: 994px;
+    right: 0;
+    top: 0;
+    background: white;
   `,
 };
