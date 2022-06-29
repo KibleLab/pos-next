a19646c6f70c1328ed84936074138fe3da18c657
RegistryROK <registry.forbusiness@gmail.com>
2022-06-30 04:40:01 +0900

    feat: title, list, info를 menu-slct/:table애 추가
    BtnMenu를 map함수로 뿌려놓음.

--- pages/menu-slct/[table].tsx
+++ pages/menu-slct/[table].tsx
@@ -1,8 +1,17 @@
 /** @jsxImportSource @emotion/react **/
 import { css } from '@emotion/react';
-import { AppBar, Button, Container } from '@mui/material';
+import { AppBar, Button, Container, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
+import BtnMenu from '../../components/BtnMenu';
+
+// dummy
+const menu = [
+  { menu_name: 'AAA', menu_price: 1000, menu_stock: 20 },
+  { menu_name: 'BBB', menu_price: 2000, menu_stock: 20 },
+  { menu_name: 'CCC', menu_price: 3000, menu_stock: 20 },
+  { menu_name: 'DDD', menu_price: 4000, menu_stock: 20 },
+];
 
 export default function MenuSlct() {
   const router = useRouter();
@@ -22,7 +31,27 @@ export default function MenuSlct() {
           <Image src='/images/back.svg' width={24} height={24} />
         </Button>
       </AppBar>
-      <Container css={styles.contents} maxWidth={false} disableGutters />
+      <Container css={styles.contents} maxWidth={false} disableGutters>
+        <Typography css={styles.title}>메뉴 선택 – Table {table}</Typography>
+        <Container css={styles.list} maxWidth={false} disableGutters>
+          <Container css={styles.btnMenuList} maxWidth={false} disableGutters>
+            {(() => {
+              return menu.map((data, index) => (
+                <div key={index}>
+                  <BtnMenu
+                    index={index}
+                    menuName={data.menu_name}
+                    menuPrice={data.menu_price}
+                    menuStock={data.menu_stock}
+                    onClick={() => {}}
+                  />
+                </div>
+              ));
+            })()}
+          </Container>
+        </Container>
+        <Container css={styles.info} maxWidth={false} disableGutters />
+      </Container>
     </>
   );
 }
@@ -72,5 +101,36 @@ const styles = {
     height: 994px;
     left: 0;
     top: 86px;
+  `,
+  title: css`
+    position: absolute;
+    left: 32px;
+    top: 30px;
+    font-size: 36px;
+    font-family: Roboto;
+  `,
+  list: css`
+    position: absolute;
+    width: 1260px;
+    height: 920px;
+    left: 0;
+    bottom: 0;
+  `,
+  btnMenuList: css`
+    position: absolute;
+    width: 1220px;
+    height: 856px;
+    left: 32px;
+    bottom: 32px;
+    overflow-x: hidden;
+    overflow-y: auto;
+  `,
+  info: css`
+    position: absolute;
+    width: 660px;
+    height: 994px;
+    right: 0;
+    top: 0;
+    background: white;
   `,
 };
